f63a49264d81acb75afdbcd83ae642ded7285182
ljs <jslim@banaple.co.kr>
2025-03-10 09:01:59 +0900

    no message

--- src/page/Detail.tsx
+++ src/page/Detail.tsx
@@ -1,11 +1,9 @@
 import styled from "styled-components";
-// import Horizontal from "../components/Horizontal";
 import data from "../assets/data/data";
 
 const Detail = () => {
   return (
     <DetailWrap>
-      {/* <Horizontal /> */}
       {data.map((items, index) => (
         <p key={index}>{items.title}</p>
       ))}
